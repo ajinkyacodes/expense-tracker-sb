722fe60c93b0a44b4a47f3546e4d6776ca755f0a
AJINKYA <ajinkyacodes@gmail.com>
2025-07-10 17:35:19 +0530

    created CategoryController and built create category REST API

--- src/main/java/com/ajinkyacodes/expense_tracker/controller/CategoryController.java
+++ src/main/java/com/ajinkyacodes/expense_tracker/controller/CategoryController.java
@@ -1,4 +1,25 @@
 package com.ajinkyacodes.expense_tracker.controller;
 
+import com.ajinkyacodes.expense_tracker.dto.CategoryDto;
+import com.ajinkyacodes.expense_tracker.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/categories") // Define the base URL
 public class CategoryController {
+    private CategoryService categoryService;
+
+    // Build Create Category REST API
+    @PostMapping
+    public ResponseEntity<CategoryDto> createCategory(@RequestBody CategoryDto categoryDto) {
+        CategoryDto category = categoryService.createCategory(categoryDto);
+        return new ResponseEntity<>(category, HttpStatus.CREATED);
+    }
 }
