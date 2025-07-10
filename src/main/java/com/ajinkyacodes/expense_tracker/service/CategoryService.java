81038ea1882351c8fffc077e605cbf57fcc1daf1
AJINKYA <ajinkyacodes@gmail.com>
2025-07-10 17:34:44 +0530

    Created CategoryService and CategoryServiceImpl for mapping DTO to Entity and Entity to DTO

---
+++ src/main/java/com/ajinkyacodes/expense_tracker/service/CategoryService.java
@@ -0,0 +1,8 @@
+package com.ajinkyacodes.expense_tracker.service;
+
+import com.ajinkyacodes.expense_tracker.dto.CategoryDto;
+
+public interface CategoryService {
+
+    CategoryDto createCategory(CategoryDto categoryDto);
+}
---
+++ src/main/java/com/ajinkyacodes/expense_tracker/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,29 @@
+package com.ajinkyacodes.expense_tracker.service.impl;
+
+import com.ajinkyacodes.expense_tracker.dto.CategoryDto;
+import com.ajinkyacodes.expense_tracker.entity.Category;
+import com.ajinkyacodes.expense_tracker.mapper.CategoryMapper;
+import com.ajinkyacodes.expense_tracker.repository.CategoryRepository;
+import com.ajinkyacodes.expense_tracker.service.CategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public CategoryDto createCategory(CategoryDto categoryDto) {
+
+        // Convert CategoryDto to Category entity
+        Category category = CategoryMapper.mapToCategory(categoryDto);
+
+        // Save category object into database table - categories
+        Category savedCategory = categoryRepository.save(category);
+
+        // convert savedCategory to CategoryDto
+        return CategoryMapper.mapToCategoryDto(savedCategory);
+    }
+}
