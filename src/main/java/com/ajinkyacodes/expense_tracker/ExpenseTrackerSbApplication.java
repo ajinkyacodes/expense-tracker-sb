53b01fbc32fa1657325e0b27e9f4bb694b42af16
AJINKYA <ajinkyacodes@gmail.com>
2025-07-10 16:47:02 +0530

    Added Spring boot Initializr setup files

---
+++ src/main/java/com/ajinkyacodes/expense_tracker/ExpenseTrackerSbApplication.java
@@ -0,0 +1,13 @@
+package com.ajinkyacodes.expense_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerSbApplication.class, args);
+	}
+
+}
