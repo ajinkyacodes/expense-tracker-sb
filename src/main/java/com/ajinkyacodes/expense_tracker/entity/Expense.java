99f65ef52b8092c2c531fd6e97e284ab6436ced4
AJINKYA <ajinkyacodes@gmail.com>
2025-07-11 13:44:41 +0530

    created Expense JPA entity for creating table=expenses in the database

---
+++ src/main/java/com/ajinkyacodes/expense_tracker/entity/Expense.java
@@ -0,0 +1,33 @@
+package com.ajinkyacodes.expense_tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "expenses")
+public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private LocalDate expenseDate;
+
+    // many to one relationship - Many expenses belongs to one category
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false) // Foreign Key in expenses table
+    private Category category;
+}
